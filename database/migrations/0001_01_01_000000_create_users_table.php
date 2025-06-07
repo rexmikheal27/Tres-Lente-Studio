8d9cbe5972c7bba92e9d8cda58f5cf0a2c69d3cd
rexmikheal27 <veradio59@gmail.com>
2025-06-07 16:03:55 +0800

    migration database updated

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->boolean('is_active')->default(true);
-            $table->timestamp('last_login')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
